5be6f9e19ab64e338a4313d62fcc9f210cff23e0
Sangwon Choi <sangwonchoi7@gmail.com>
2015-07-21 22:23:22 +0900

    Update drv2605.ino

--- drv2605.ino
+++ drv2605.ino
@@ -9,21 +9,21 @@ void setup() {
   Serial.println("DRV test");
     drv.begin();
     drv.setMode(DRV2605_MODE_PWMANALOG);
-
+    drv.writeRegister8(0x1A, 0x76);
     pinMode(PWM_PIN, OUTPUT);   // sets the pin as output
 }
 
 void loop() {
   // open loop p26 
   drv.writeRegister8(0x1D, 0x01); // N_PWM_ANALOG = 0, LRA_OPEN_LOOP=1
-//  drv.writeRegister8(0x1A, 0x76);
+  drv.writeRegister8(0x1A, 0xB6);
   readRegister();
   Serial.println("open loop");
   beep();
   
   // close loop
   drv.writeRegister8(0x1D, 0x00); // N_PWM_ANALOG = 0, LRA_OPEN_LOOP=0 (auto resonance mode)
-//  drv.writeRegister8(0x1A, 0x36);
+  drv.writeRegister8(0x1A, 0x36);
   readRegister();
   Serial.println("close loop");
   beep(); 
@@ -37,7 +37,7 @@ void readRegister() {
 
 void beep() {
 
-   analogWrite(PWM_PIN, 125); 
+   analogWrite(PWM_PIN, 130); 
    delay(1000);
   analogWrite(PWM_PIN, 50); 
    delay(1000);
