2f7d3b76803d65ac9e847cdd6c04536d0d5b5f3c
Sangwon Choi <sangwonchoi7@gmail.com>
2015-07-21 22:09:52 +0900

    Create drv2605.ino

---
+++ drv2605.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+#include "Adafruit_DRV2605.h"
+
+Adafruit_DRV2605 drv;
+int PWM_PIN = 3;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DRV test");
+    drv.begin();
+    drv.setMode(DRV2605_MODE_PWMANALOG);
+
+    pinMode(PWM_PIN, OUTPUT);   // sets the pin as output
+}
+
+void loop() {
+  // open loop p26 
+  drv.writeRegister8(0x1D, 0x01); // N_PWM_ANALOG = 0, LRA_OPEN_LOOP=1
+//  drv.writeRegister8(0x1A, 0x76);
+  readRegister();
+  Serial.println("open loop");
+  beep();
+  
+  // close loop
+  drv.writeRegister8(0x1D, 0x00); // N_PWM_ANALOG = 0, LRA_OPEN_LOOP=0 (auto resonance mode)
+//  drv.writeRegister8(0x1A, 0x36);
+  readRegister();
+  Serial.println("close loop");
+  beep(); 
+}
+void readRegister() {
+    Serial.print(drv.readRegister8(0x1A));
+    Serial.print(" ");
+    Serial.print(drv.readRegister8(0x1D));
+    Serial.println();
+}
+
+void beep() {
+
+   analogWrite(PWM_PIN, 125); 
+   delay(1000);
+  analogWrite(PWM_PIN, 50); 
+   delay(1000);
+   
+}
